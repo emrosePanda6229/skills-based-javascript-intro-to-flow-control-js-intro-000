6123ef7b41916cb86080f3cddd95ded13d834e34
Emily R Collazo <ec889x@att.com>
2020-02-10 19:08:52 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -12,6 +12,14 @@ function teenager(age) {
   }
 }
 
+function teenager(age) {
+  if (age > 13 && < 19) {
+    "You are a teenager"
+  } else {
+    "You are not a teenager"
+  }
+}
+
 function ternaryTeenager(age) {
 
 }
